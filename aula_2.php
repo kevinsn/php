05717867ea2aee6bcd3f7fb4df73837b7da52c7d
Kevin <kevinsadao@hotmail.com>
2021-02-15 20:50:02 -0300

    Add aula 2 com tipos de dados e comentários

---
+++ aula_2.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+Comenta + de 1 linha
+*/
+
+// Comenta apenas uma linha
+
+$nome = "Kevin";
+echo $nome;
+
+// Tipos de dados:
+/* 
+Inteiro;
+Ponto flutuante;
+String;
+Array;
+Objeto;
+Booleanos
+*/
+
+$nome = "Kevin";
+$idade = 35;
+$pi = 3.14;
+$acesso = true;
+
+echo $nome."<br>";
+echo $idade."<br>";
+echo $pi."<br>";
+echo $acesso."<br>";
+
+?>
