32e82b44a50d2014fbdb8949cd7c9793c37b610a
Kevin <kevinsadao@hotmail.com>
2021-03-01 22:18:00 -0300

    For e ForEach

---
+++ exemploFor.php
@@ -0,0 +1,15 @@
+<?php
+
+// for ($xpto = 1; $xpto <= 1000; $xpto++){
+//   echo $xpto."<br>";
+// }
+
+for ($x = 40; $x > 20; $x--){
+  if ($x % 2 == 1){
+    echo $x."<br>";
+  }
+}
+
+echo "Fim";
+
+?>
---
+++ exemploForEach.php
@@ -0,0 +1,19 @@
+<?php
+
+$nomes = array("Francisco", "João", "José", "Maria", "Rosa", "Fulano");
+
+foreach($nomes as $nome){
+  echo($nome."<br>");
+}
+
+// for($x = 0; $x < 5; $x++){
+//   echo $nomes[$x]."<br>";
+// }
+
+$numeros = array(-1, 0, 1, 2, 3);
+
+foreach($numeros as $numero){
+  echo($numero."<br>");
+}
+
+?>
