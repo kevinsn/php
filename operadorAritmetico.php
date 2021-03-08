d774b67584badc43eb0aceebd1c52f0042d9c2b3
Kevin <kevinsadao@hotmail.com>
2021-03-08 19:31:45 -0300

    Alterado nome do arquivo operador.php

---
+++ operadorAritmetico.php
@@ -0,0 +1,23 @@
+<?php
+
+  // Operadores Aritméticos
+
+  // + Adição
+  // - Subtração
+  // * Multiplicação
+  // / Divisão
+  // % Módulo
+  // ** Exponencial
+
+  // +$a	Identidade Conversão de $a para int ou float conforme apropriado.
+  // -$a	Negação	Oposto de $a.
+
+  $numero1 = 10;
+  $numero2 = 3;
+  $resultado = $numero1 * $numero2;
+
+  // Texto concatenando com uma variável, utilizar o ponto (.)
+
+  echo "Resultado: ".$resultado 
+
+?>
